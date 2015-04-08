e95ac12915407cfc751c0a90070ef145d1707aa6
unknown <101944@CAL-LTP-DL171.cts.com>
2015-04-08 18:49:42 +0530

    test7

--- src/test/java/devops/junit/TestBankAccount.java
+++ src/test/java/devops/junit/TestBankAccount.java
@@ -3,7 +3,7 @@ package devops.junit;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import main.java.devops.test;
+import Devops.src.main.java.devops.bank;
  
 public class TestBankAccount {
 
