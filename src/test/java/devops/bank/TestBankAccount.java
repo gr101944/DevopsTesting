f1a114096b46d619a12e709c344c520bc48272d2
unknown <101944@CAL-LTP-DL171.cts.com>
2015-04-09 11:30:44 +0530

    changed value of the method

--- src/test/java/devops/bank/TestBankAccount.java
+++ src/test/java/devops/bank/TestBankAccount.java
@@ -21,7 +21,7 @@ public class TestBankAccount {
 	public void testDebitWithInsufficientFunds()
 	{
 		BankAccount account =  new BankAccount(10);
-		double amount = account.debit(11);
+		double amount = account.debit(12);
 		Assert.assertEquals(10.0, amount);
 
 	}
